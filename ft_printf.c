6fb61b42ee248d5603ebfd2d87e8ad05cddd1acc
Kim <162928080+kimnguye@users.noreply.github.com>
2024-10-22 14:41:45 +0200

    Ready for exam rank03
    Penser a mettre un long en argument de la fonction ft_putnbr

--- ft_printf.c
+++ ft_printf.c
@@ -3,28 +3,25 @@
 int	ft_putstr(char *str);
 int	ft_printf(const char *s, ...);
 
-int	ft_putnbr(int nb, int code)
+int	ft_putnbr(long nb, int base)
 {
-	char *base = "0123456789abcdef";
+	char *b = "0123456789abcdef";
 
-	if (nb == -2147483647 && code == 10)
-		return (ft_putstr("-2147483647"));
-	if (nb < 0 && code == 10)
-		return(write(1, "-", 1) + ft_putnbr(-nb, 10));
-	if (nb < 0 && code == 16)
-	{
-		return (write(1, "test\n", 5));
-	}
-		// return (ft_putnbr((unsigned int) nb, 16));
-	if (nb / code == 0)
-		return (write(1, &base[nb % code], 1));
-	return (ft_putnbr(nb / code, code) + ft_putnbr(nb % code, code));
+	if (nb < 0 && base == 10)
+		return(write(1, "-", 1) + ft_putnbr(-nb, base));
+	if (nb < 0 && base == 16)
+		return (ft_putnbr((unsigned int) nb, base));
+	if (nb / base == 0)
+		return (write(1, &b[nb], 1), 1);
+	return (ft_putnbr(nb / base, base) + ft_putnbr(nb % base, base));
 }
 
 int	ft_putstr(char *str)
 {
 	if (!str)
 		return (write(1, "(null)", 6));
+		if (!*str)
+		return (0);
 	return (write(1, str, 1) + ft_putstr(str + 1));
 }
 
@@ -58,15 +55,33 @@ int	main(void)
 	printf("%i\n", printf(""));
 	printf("%i\n", printf(NULL));
 	printf("%i\n", printf("%s\n", (char *)NULL));
-	printf("%i\n", printf("%x\n", -2147483647));
-	printf("%i\n", printf("%d\n", -2147483647));
+	printf(" || %d\n", printf("%x", -2147483647));
+	printf(" || %d\n", printf("%d", -2147483647));
+	printf(" || %d\n", printf("%x %x %x %x", 10, 9, 16, 17));
+	printf(" || %d\n", printf("%d %d %d %d", 10, 9, 16, 17));
+	printf("%d\n", printf("%x\n", 21));
+	printf("%d\n", printf("%d\n", 21));
+	printf("%d\n", printf("%x\n", 2147483647));
+	printf("%d\n", printf("%d\n", 2147483647));
+	printf("%d\n", printf("coucou comment tu %s\n", "vas"));
+	printf("%d\n", printf("%d\n", -20));
+	printf("%d\n", printf("%x\n", -2));
 
-	ft_printf("ft_printf now:\n");
+	ft_printf("\n**********************\n\n");
+
 	ft_printf("%d\n", ft_printf(""));
 	ft_printf("%d\n", ft_printf(NULL));
 	ft_printf("%d\n", ft_printf("%s\n", (char *)NULL));
-	ft_printf("%x\n", -2147483647);
-	// ft_printf("%d\n", ft_printf("%x\n", -2147483647));
-	// ft_printf("%d\n", ft_printf("%d\n", -2147483647));
+	ft_printf(" || %d\n", ft_printf("%x", -2147483647));
+	ft_printf(" || %d\n", ft_printf("%d", -2147483647));
+	ft_printf(" || %d\n", ft_printf("%x %x %x %x", 10, 9, 16, 17));
+	ft_printf(" || %d\n", ft_printf("%d %d %d %d", 10, 9, 16, 17));
+	ft_printf("%d\n", ft_printf("%x\n", 21));
+	ft_printf("%d\n", ft_printf("%d\n", 21));
+	ft_printf("%d\n", ft_printf("%x\n", 2147483647));
+	ft_printf("%d\n", ft_printf("%d\n", 2147483647));
+	ft_printf("%d\n", ft_printf("coucou comment tu %s\n", "vas"));
+	ft_printf("%d\n", ft_printf("%d\n", -20));
+	ft_printf("%d\n", ft_printf("%x\n", -2));
 	return (0);
 }
